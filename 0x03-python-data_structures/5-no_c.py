a43d5d4cc51f0b098e2ba0c347df3895297f78f7
minarob23 <menarobir246rm@gmail.com>
2023-08-13 18:08:41 +0000

    mina

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,8 @@
 def no_c(my_string):
     new_string=""
     for char in my_string:
-        if char == 'c' or char == 'C':
+        if char=='c' or char=='C':
             pass
         else:
-            new_string +=char
+            new_string+=char
     return new_string
