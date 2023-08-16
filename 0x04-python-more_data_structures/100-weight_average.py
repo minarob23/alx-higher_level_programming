12cf954b5a502b2f46e533dd13837f95f9ae30df
minarob23 <menarobir246rm@gmail.com>
2023-08-16 20:27:44 +0000

    mina

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,9 +2,12 @@
 def weight_average(my_list=[]):
     if not my_list:
         return 0
+    
     total_weighted_score = 0
     total_weight = 0
-    for avg in my_list:
-        total_weighted_score += avg[0] * avg[1]
-        total_weight += avg[1]    
+    
+    for score, weight in my_list:
+        total_weighted_score += score * weight
+        total_weight += weight
+    
     return total_weighted_score / total_weight
