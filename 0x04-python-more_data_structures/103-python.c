3ef85c36ee5eed913f6f99484f48aa595733ae82
minarob23 <menarobir246rm@gmail.com>
2023-08-16 20:52:57 +0000

    mina

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,21 +1,19 @@
-#include <stdio.h>
 #include <Python.h>
 
 /**
- * print_python_list - Prints list information
- *
- * @p: Python Object
- * Return: no return
+ * print_python_list - Prints basic information about a Python list object.
+ * @p: The PyObject representing the Python list.
  */
 void print_python_list(PyObject *p) {
     printf("[*] Python list info\n");
 
     if (PyList_Check(p)) {
-        printf("[*] Size of the Python List = %ld\n", PyList_Size(p));
+        Py_ssize_t size = PyList_Size(p);
+        printf("[*] Size of the Python List = %ld\n", size);
 
         printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
-        
-        for (Py_ssize_t i = 0; i < PyList_Size(p); ++i) {
+
+        for (Py_ssize_t i = 0; i < size; ++i) {
             PyObject *item = PyList_GET_ITEM(p, i);
             printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
         }
@@ -25,23 +23,23 @@ void print_python_list(PyObject *p) {
 }
 
 /**
- * print_python_bytes - Prints bytes information
- *
- * @p: Python Object
- * Return: no return
+ * print_python_bytes - Prints basic information about a Python bytes object.
+ * @p: The PyObject representing the Python bytes object.
  */
 void print_python_bytes(PyObject *p) {
     printf("[.] bytes object info\n");
 
     if (PyBytes_Check(p)) {
-        printf("  size: %ld\n", ((PyVarObject *)p)->ob_size);
+        Py_ssize_t size = ((PyVarObject *)p)->ob_size;
+        printf("  size: %ld\n", size);
+
         printf("  trying string: %s\n", PyBytes_AS_STRING(p));
 
-        printf("  first %ld bytes: ", ((PyVarObject *)p)->ob_size + 1);
+        printf("  first %ld bytes: ", size + 1);
 
-        for (Py_ssize_t i = 0; i <= ((PyVarObject *)p)->ob_size && i < 10; ++i) {
+        for (Py_ssize_t i = 0; i <= size && i < 10; ++i) {
             printf("%02hhx", ((char *)p)[i]);
-            if (i < ((PyVarObject *)p)->ob_size) {
+            if (i < size) {
                 printf(" ");
             }
         }
