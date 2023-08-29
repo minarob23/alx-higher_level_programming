f9463d7a4de06b51db9aab5af1ab9d423b96bc5f
minarob23 <menarobir246rm@gmail.com>
2023-08-29 01:36:31 +0000

    mina

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,9 +2,11 @@
 import sys
 
 def safe_function(fct, *args):
-    result = None
     try:
         result = fct(*args)
     except Exception as e:
         print("Exception:", e, file=sys.stderr)
-    return result
+        result = None
+    finally:
+        return result
+
