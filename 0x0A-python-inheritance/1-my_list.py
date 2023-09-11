c19b3c21be51b64ee606b820e9cf2597d8abd33d
minarob23 <menarobir246rm@gmail.com>
2023-09-11 15:03:47 +0000

    mina

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-"""A custom list class that inherits from the built-in list class."""
+"""
+contains the MyList class
+"""
+
+
 class MyList(list):
+    """a subclass of list"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
+
     def print_sorted(self):
-        """
-        Prints the list in ascending sorted order without modifying the original list.
-        """
-        sorted_list = sorted(self)
-        print(sorted_list)
+        """prints the sorted list"""
+        print(sorted(self))
